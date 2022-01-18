0641a4abb07f035034aa65252d421062b1f54380
GWSG <GWSG@10.131.153.66>
2022-01-19 08:53:59 +0900

    for문 7개 진행중

---
+++ backjuntest/src/backjuntest/for문/Test2742.java
@@ -0,0 +1,36 @@
+/*문제
+자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩(큰 숫자부터) 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+
+출력
+첫째 줄부터 N번째 줄 까지 차례대로 출력한다.*/
+
+package backjuntest.for문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Test2742 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = N; i >= 1; i--) {
+			
+			
+			bw.write(i+"\n");
+		}
+		bw.close();
+	}
+	
+}
