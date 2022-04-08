1b6a57f464662a8732e3b6a6904e178baf1fc327
GWSG김성년 <GWSG@10.131.153.66>
2022-04-08 16:58:07 +0900

    [22.04.08] 2

--- backjuntest/src/backjuntest/배열/Test2562.java
+++ backjuntest/src/backjuntest/배열/Test2562.java
@@ -1,3 +1,18 @@
+/*문제
+9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.
+
+예를 들어, 서로 다른 9개의 자연수
+
+3, 29, 38, 12, 57, 74, 40, 85, 61
+
+이 주어지면, 이들 중 최댓값은 85이고, 이 값은 8번째 수이다.
+
+입력
+첫째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100 보다 작다.
+
+출력
+첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.*/
+
 package backjuntest.배열;
 
 import java.util.Scanner;
@@ -5,29 +20,32 @@ import java.util.Scanner;
 public class Test2562 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		
-		int [] arr = null;
-		
-		for(int i= 0; i<101; i++) {
-			if( sc.hasNextInt()) {
-				break;
-			}else{
-			arr[i] = sc.nextInt();
-			}
-		}
+		int [] arr = new int[9];
+		int length = arr.length;
 		int max = 0;
-		for(int j = 0; j < arr.length; j++) {
-			if(arr[j] >= max) {
-				max = arr[j];
+		int position = 0 ;
+		
+		for (int i = 0; i < length; i++) {
+			 arr[i] =	sc.nextInt();
+			  
+			 if (max <= arr[i]) {
+				 max = arr[i];
+			 }
+		for(int j = 0; j < length; j++) {
+			if (arr[j] == max) {
+				 position = j + 1; 
 			}
-		System.out.println(max);	
-		}
-		for(int j = 0; j < arr.length; j++) {
-			if(arr[j] == max) {
-				System.out.print(j);
-			}
+		} 	 
+			 
+		
 		}
 		
+		System.out.println(max);
+		System.out.println(position);
+		
+		
+		
+		
+		sc.close();
 	}
 }
---
+++ backjuntest/src/backjuntest/배열/Test2577.java
@@ -0,0 +1,88 @@
+/*문제
+세 개의 자연수 A, B, C가 주어질 때 A × B × C를 계산한 결과에 0부터 9까지 각각의 숫자가 몇 번씩 쓰였는지를 구하는 프로그램을 작성하시오.
+
+예를 들어 A = 150, B = 266, C = 427 이라면 A × B × C = 150 × 266 × 427 = 17037300 이 되고, 계산한 결과 17037300 에는 0이 3번, 1이 1번, 3이 2번, 7이 2번 쓰였다.
+
+입력
+첫째 줄에 A, 둘째 줄에 B, 셋째 줄에 C가 주어진다. A, B, C는 모두 100보다 크거나 같고, 1,000보다 작은 자연수이다.
+
+출력
+첫째 줄에는 A × B × C의 결과에 0 이 몇 번 쓰였는지 출력한다. 마찬가지로 둘째 줄부터 열 번째 줄까지 A × B × C의 결과에 1부터 9까지의 숫자가 각각 몇 번 쓰였는지 차례로 한 줄에 하나씩 출력한다.
+*/
+package backjuntest.배열;
+
+import java.util.Scanner;
+
+public class Test2577 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		int zero = 0;
+		int one = 0;
+		int two = 0;
+		int three = 0;
+		int four = 0;
+		int five = 0;
+		int six = 0;
+		int seven = 0;
+		int eight = 0;
+		int nine = 0;
+		
+		int sum = A * B * C;
+		int length = (int)(Math.log10(sum)+1);
+		
+		String strSum = Integer.toString(sum);
+		String arr[] = new String[length];
+		
+		for (int i = 0; i<length; i++) {
+			arr[i] = strSum.substring(i,i+1);
+			System.out.println("arr["+i+"]:" + arr[i]);
+		}
+		for (int j = 0; j < length; j++) {
+			if (arr[j].equals("0")) {
+				zero = zero + 1;
+			}else if (arr[j].equals("1")) {
+				one = one + 1;
+			}else if (arr[j].equals("2")) {
+				two = two + 1;
+			}else if (arr[j].equals("3")) {
+				three = three + 1;
+			}else if (arr[j].equals("4")) {
+				four = four + 1;
+			}else if (arr[j].equals("5")) {
+				five = five + 1;
+			}else if (arr[j].equals("6")) {
+				six = six + 1;
+			}else if (arr[j].equals("7")) {
+				seven = seven + 1;
+			}else if (arr[j].equals("8")) {
+				eight = eight + 1;
+			}else if (arr[j].equals("9")) {
+				nine = nine + 1;
+			}
+			
+		}
+		
+		System.out.println(zero);
+		System.out.println(one);
+		System.out.println(two);
+		System.out.println(three);
+		System.out.println(four);
+		System.out.println(five);
+		System.out.println(six);
+		System.out.println(seven);
+		System.out.println(eight);
+		System.out.println(nine);
+		
+		
+		
+		
+		sc.close();	
+	}
+	
+
+}
